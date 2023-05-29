8b71a3682460830b8b2d6f032c8beaa1b076183c
fremydev <fremy.dev@gmail.com>
2023-05-28 21:14:29 -0300

    refactors exercise 1-9.

--- chapter-1/1-9.c
+++ chapter-1/1-9.c
@@ -2,18 +2,12 @@
 
 /* write a program that replaces multiple blankspaces to a single one */
 main () {
-	int c, pblank;
-	pblank = -1;
+	int c, pc;
+	pc = -1;
 	while ((c = getchar()) != EOF) {
-		if (c == ' ' && pblank != 1) {
-			pblank = 1;
+		if (c != ' ' || pc != ' ') {
 			putchar(c);
 		}
-		else if (c == ' ' && pblank == 1)
-			pblank = 1;
-		else {
-			pblank = 0;
-			putchar(c);
-		}
+		pc = c;
 	}
 }
