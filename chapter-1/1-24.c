d4018d105d87d3c3c796e4b7e7090868d214c2e3
fremydev <fremy.dev@gmail.com>
2023-06-11 18:22:44 -0300

    add exercise 1-24. placeholder

---
+++ chapter-1/1-24.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#define MAXSIZE 1000
+
+int leftbracket;
+int leftparenthese;
+int left brace;
+
+int rightbracket;
+int rightparanthese;
+int rightbrace;
+
+int lineget(char line[], int maxsize);
+void isbalanced(char line[]);
+void printinfo(void);
+
+int main(void) {
+	int c;
+	char line[];
+
+	while (lineget(line, maxsize) > 0)
+		isbalanced(line);	
+
+	printinfo();
+
+	return 0;
+}
+
+int lineget(char s[], int lim) {
+	
+}
+
+void isbalanced(char line[]) {
+
+}
+
+void printinfo(void) {
+
+}
