9354fcd9734020cd929e627514c6bebb0749e7f8
fremydev <fremy.dev@gmail.com>
2023-06-18 17:39:24 -0300

    adds exercise 3-1.

---
+++ chapter-3/3-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int binarysearch(int x, int v[], int n);
+
+int main(void) {
+    return 0;
+}
+
+int binarysearch(int x, int v[], int n) {
+    int low, high, mid;
+    low = 0;
+    high = n-1;
+    while(low <= high) {
+        mid = (low+high)/2;
+        if (x <= v[mid]) high = mid - 1;
+        else low = mid + 1;
+    }
+    return (x == v[low]) ? low : -1;
+}
