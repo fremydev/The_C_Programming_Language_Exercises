07c18c78a827422cff88f0f0bbe50230b1b3ab41
fremydev <fremy.dev@gmail.com>
2023-06-22 19:41:16 -0300

    adds exercise 3-4.

---
+++ chapter-3/3-4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void itoa(unsigned n, char s[]);
+void reverse(char s[]);
+
+int main(void) {
+    return 0;
+}
+
+void itoa(unsigned n, char s[]) {
+    int i, sign;
+    if ((sign = n) < 0) n = -n;
+    i = 0;
+    do s[i++] = n % 10 + '0';
+    while ((n /= 10) > 0);
+
+    if (sign < 0) s[i++] = '-';
+    s[i] = '\0';
+
+    reverse(s);
+}
+
